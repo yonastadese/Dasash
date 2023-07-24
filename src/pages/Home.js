9145ea18e34f40a78e9e2cc2de58271b3e205d11
yonastadese <76045908+yonastadese@users.noreply.github.com>
2023-07-25 00:34:13 +0300

    first check for dashboard

---
+++ src/pages/Home.js
@@ -0,0 +1,34 @@
+// @mui
+import { styled } from '@mui/material/styles';
+// components
+import Page from '../components/Page';
+// sections
+import {
+  HomeHero,
+  
+} from '../sections/home';
+
+// ----------------------------------------------------------------------
+
+const RootStyle = styled('div')(() => ({
+  height: '100%',
+}));
+
+const ContentStyle = styled('div')(({ theme }) => ({
+  overflow: 'hidden',
+  position: 'relative',
+  backgroundColor: theme.palette.background.default,
+}));
+
+// ----------------------------------------------------------------------
+
+export default function HomePage() {
+  return (
+    <Page title="The starting point for your next project">
+      <RootStyle>
+        <HomeHero />
+       
+      </RootStyle>
+    </Page>
+  );
+}
